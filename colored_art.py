25a5500e13acfba976c0df4e8626ab9a61ea9ab0
Brouq <plialq.077@gmail.com>
2025-10-19 10:41:16 +0300

    done

---
+++ colored_art.py
@@ -0,0 +1,10 @@
+from art import *
+from colorama import Fore, Style, init
+
+init(autoreset=True)
+print(Fore.RED + text2art("BELIEVE", font="block"))
+print(Fore.GREEN + text2art("AND", font="digital"))
+print(Fore.BLUE + text2art("ACHIEVE", font="block"))
+
+print(Fore.MAGENTA + text2art("HELLO", font="sub-zero"))
+print(Fore.CYAN + text2art("WORLD", font="doom"))
