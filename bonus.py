a38e15c6bcaf36c4d27c93f423e1cf301de87a53
abdulmajeed <mjeed.f.b@outlook.sa>
2025-10-19 10:49:21 +0300

    pip lab

---
+++ bonus.py
@@ -0,0 +1,8 @@
+from colorama import Fore, Back, Style
+
+
+
+
+print(Back.LIGHTBLUE_EX + "BELIEVE AND ACHEIVE")
+print(Back.BLACK)
+print(Fore.GREEN + Back.BLACK + "hello")
---
+++ text.py
@@ -0,0 +1,7 @@
+from art import text2art
+
+
+txt1 = text2art("BELIEVE AND ACHEIVE", font = "block")
+txt2 = text2art("hello", font = "sub-zero")
+print(txt1)
+print(txt2)
