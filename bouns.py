3a9eb811556de7fc378bd36295225baf840a0447
lama <leema442@gmail.com>
2025-10-19 10:55:01 +0300

    modfy

--- bouns.py
+++ bouns.py
@@ -1,10 +1,14 @@
 from colorama import Fore, Back, Style
 from colorama import init
-
+from art import text2art 
+from art import *
 
 init(autoreset=True)
-print(Fore.LIGHTBLUE_EX + 'Hello Word')
-print(Fore.BLUE + 'My name is Lama')
+print(Fore.LIGHTBLUE_EX +text2art('Hello Word',font='cybermedium'))
+print(Fore.BLUE + 'My name is :')
+tprint("Lama")
 print(Back.CYAN + 'Graduted at computer Scince')
 print(Style.DIM + 'Study web devlop by python')
-print('Thank you')
+print(Fore.LIGHTBLUE_EX +text2art('Thank you','rand'))
+
+
