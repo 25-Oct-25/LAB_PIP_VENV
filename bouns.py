2e460d71bcc9454c6a5bdcd80c7f1ef6b78815be
lama <leema442@gmail.com>
2025-10-19 10:37:59 +0300

    add lab pip

---
+++ bouns.py
@@ -0,0 +1,10 @@
+from colorama import Fore, Back, Style
+from colorama import init
+
+
+init(autoreset=True)
+print(Fore.LIGHTBLUE_EX + 'Hello Word')
+print(Fore.BLUE + 'My name is Lama')
+print(Back.CYAN + 'Graduted at computer Scince')
+print(Style.DIM + 'Study web devlop by python')
+print('Thank you')
---
+++ main.py
@@ -0,0 +1,12 @@
+from art import text2art
+
+
+
+phrase1=text2art("BELIEVE",font='block')
+phrase2=text2art("AND",font='block')
+phrase3=text2art("ACHEIVE",font='block')
+
+
+print(f"{phrase1}\n {phrase2} \n {phrase3}")
+phrase4=text2art("HELLO",font='sub-zero')
+print(phrase4)
