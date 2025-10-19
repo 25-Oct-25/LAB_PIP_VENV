e99b62b71a49a1da70b4cec1ef8cd6f287d04053
Hissah Saad <hissahsaad23@gmail.com>
2025-10-19 11:16:26 +0300

    bonus is done

---
+++ bonus.py
@@ -0,0 +1,9 @@
+from art import *
+from colorama import Fore, Back, Style
+
+tprint("fighting!", font="rand")
+tprint("You Can Do it!", font = "random")
+tprint("Game Over")
+
+text = Fore.WHITE + Back.GREEN +Style.DIM +"Game Over"
+print(text)
