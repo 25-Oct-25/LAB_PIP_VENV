ef6a4a8f5ad327fe40195a304f56f7b5babc2ee8
Hissah Saad <hissahsaad23@gmail.com>
2025-10-19 09:59:15 +0300

    lab is done - Hissah Alkharboush

---
+++ main.py
@@ -0,0 +1,9 @@
+from art import *
+
+#create variables
+word1 = "BELIVE AND ACHEIVE"
+word2 = "HELLO"
+
+#edit font by using ART package
+tprint(word1, font = "block")
+tprint( word2 , font = "sub-zero")
